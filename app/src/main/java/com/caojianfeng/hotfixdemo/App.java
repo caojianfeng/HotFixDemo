9c654319c81de28518f5d7a9e8d88ac0fb971b32
windcao <windcao@hotmail.com>
2018-10-06 13:44:28 +0800

    loading patch

---
+++ app/src/main/java/com/caojianfeng/hotfixdemo/App.java
@@ -0,0 +1,41 @@
+package com.caojianfeng.hotfixdemo;
+
+import android.app.Application;
+import android.os.Environment;
+import android.util.Log;
+
+import com.alipay.euler.andfix.patch.PatchManager;
+
+import java.io.File;
+import java.io.IOException;
+
+public class App extends Application {
+    private static final String TAG = "-- app --";
+    private PatchManager patchManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initHotfix();
+    }
+
+    private void initHotfix() {
+        App context = this;
+        patchManager = new PatchManager(context);
+        patchManager.init(BuildConfig.VERSION_NAME);//current version
+        patchManager.loadPatch();
+        File patchDir = new File(Environment.getExternalStorageDirectory(), "/com.caojianfeng/patchs");
+        File[] subFile = patchDir.listFiles();
+        try {
+            for (File file : subFile) {
+                String path = file.getAbsolutePath();
+                if (path.endsWith(".apatch")) {
+                    Log.i(TAG, "adding patch:" + path);
+                    patchManager.addPatch(path);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
